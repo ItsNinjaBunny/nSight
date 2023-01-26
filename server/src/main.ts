2a9dc9909a6ce415c8df362a24c5fa3d4ea91520
Brayden <Braydenlwong@gmail.com>
2023-01-25 19:13:14 -0700

    dynamic port mapping

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger(AppModule.name);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('PORT');
+  await app.listen(port, () => logger.log(`Server running on port ${port}`));
 }
 bootstrap();
